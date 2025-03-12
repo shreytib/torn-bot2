3b858c740524c8a3da4849a30fed725295d405e0
shreytib <32537478+shreytib@users.noreply.github.com>
2025-03-13 02:25:35 +0530

    1

--- try.js
+++ try.js
@@ -1,15 +1,8 @@
 const fs = require("fs");
 
 
-let stalkList = require('./stalkList.json');
 let players = require('./players.json');
 
-for (let i in stalkList){
-    if(!players.hasOwnProperty(i)){
-        console.log('Deleting : ', i);
-        delete stalkList[i];
-    }
-}
 
-fs.writeFileSync('stalkList.json', JSON.stringify(stalkList));
-console.log('Done');
+
+console.log(Object.keys(players).length);
